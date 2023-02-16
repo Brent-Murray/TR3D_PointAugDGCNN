7f9cc50eb382cbe97e678ce8c4094ffbd8abe8eb
Brent Murray <56763040+Brent-Murray@users.noreply.github.com>
2023-02-16 08:48:44 -0800

    Update send_telegram.py

--- utils/send_telegram.py
+++ utils/send_telegram.py
@@ -1,15 +1,11 @@
 import requests
 
-def send_telegram(message):
-    TOKEN = "6201723110:AAFh6na0r7FVA44Ox4c7pUd2Gzb9Xl3kbKk"
-    chat_id = "6191158308"
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+def send_telegram(message, token, chat_id):
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
     resp = requests.get(url).json()
     return resp
     
-def send_photos(file):
-    token = "6201723110:AAFh6na0r7FVA44Ox4c7pUd2Gzb9Xl3kbKk"
-    chat_id = "6191158308"
+def send_photos(file, token, chat_id):
     method = "sendPhoto"
     params = {'chat_id': chat_id}
     files = {'photo': file}
@@ -17,8 +13,3 @@ def send_photos(file):
     resp = requests.post(url + method, params, files=files)
     
     return resp
-
-if __name__ == '__main__':
-    message = "hello from your telegram bot"
-    send_telegram(message)
-    send_photos(open(r"D:\MurrayBrent\projects\TR3D\checkpoints\dgcnn_pointaugment_4096\output\confusion_matrix.png", 'rb'))
